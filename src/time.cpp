d2e4a3efe14fe11d2061ce516e0e941385182a84
Daniel Jones <McSwaggens@protonmail.com>
2016-12-27 09:59:44 +1100

    Fixed compilation for macOS

--- src/time.cpp
+++ src/time.cpp
@@ -3,12 +3,12 @@
 // TIME DATA
 uint64_t Time::last = 0;
 uint64_t Time::now = 0;
-double Time::delta = 0.0d;
+double Time::delta = 0.0;
 
 void Time::CalculateTime ()
 {
 	Time::last = Time::now;
 	Time::now = SDL_GetTicks ();
 	
-	Time::delta = (double)(Time::now - Time::last) / 1000.0d;
+	Time::delta = (double)(Time::now - Time::last) / 1000.0;
 }
