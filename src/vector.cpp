a796ca92962ef2db075432d7c6d3d55d680e9387
Daniel Jones <McSwaggens@protonmail.com>
2016-12-27 10:37:45 +1100

    Expanded vector.h

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,1 +1,11 @@
 #include "vector.h"
+
+IVector2D Vector2D::ToIVector()
+{
+	return IVector2D (x, y);
+}
+
+Vector2D IVector2D::ToVector()
+{
+	return Vector2D (x, y);
+}
--- src/vector.h
+++ src/vector.h
@@ -6,6 +6,10 @@
 #define VP(c) c.x, c.y
 #define VPI(c) (int)c.x, (int)c.y
 #define Vector Vector2D
+#define IVector IVector2D
+
+struct Vector2D;
+struct IVector2D;
 
 struct Vector2D
 {
@@ -22,6 +26,12 @@ struct Vector2D
 	{
 		this->x = x;
 		this->y = y;
+	}
+	
+	Vector2D (int x, int y)
+	{
+		this->x = (float)x;
+		this->y = (float)y;
 	}
 	
 	Vector2D operator + (const Vector2D &v) const
@@ -44,5 +54,59 @@ struct Vector2D
 		return Vector (x / v.x, y / v.y);
 	}
 	
+	IVector2D ToIVector ();
+};
+
+
+// Mainly used for internal engine methods like screen reslution etc..
+struct IVector2D
+{
+	int x;
+	int y;
 	
+	IVector2D ()
+	{
+		this->x = 0;
+		this->y = 0;
+	}
+	
+	
+	IVector2D (int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	
+	IVector2D (float x, float y)
+	{
+		this->x = (int)x;
+		this->y = (int)y;
+	}
+	
+	IVector2D (Vector2D vector)
+	{
+		*this = vector;
+	}
+	
+	IVector2D operator + (const IVector2D &v) const
+	{
+		return IVector (x + v.x, y + v.y);
+	}
+	
+	IVector2D operator - (const IVector2D &v) const
+	{
+		return IVector (x - v.x, y - v.y);
+	}
+	
+	IVector2D operator * (const IVector2D &v) const
+	{
+		return IVector (x * v.x, y * v.y);
+	}
+	
+	IVector2D operator / (const IVector2D &v) const
+	{
+		return IVector (x / v.x, y / v.y);
+	}
+	
+	Vector2D ToVector ();
 };
